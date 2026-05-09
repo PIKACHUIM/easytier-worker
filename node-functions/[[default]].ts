5fe14bce64fd779378288e7ffc26e92ba1a74169
pikachuim <pikachuim@qq.com>
2026-05-09 16:00:02 +0800

    refactor(web): change ui design

--- node-functions/[[default]].ts
+++ node-functions/[[default]].ts
@@ -3,6 +3,6 @@
  * 使用 [[default]] catch-all 路由，将所有请求转发给 Hono app 处理
  * 参考：https://pages.edgeone.ai/document/cloud-functions
  */
-import {app} from '../src/index'
+import {app} from '../src/index.tsx'
 
 export default app
